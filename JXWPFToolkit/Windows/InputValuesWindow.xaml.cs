44fc2a95ca9cc01e7bd3eeb1fab8e53cd880d35e
javixulo <aronnax.ct@gmail.com>
2016-03-18 22:46:45 +0100

    Added InputValuesWindow

---
+++ JXWPFToolkit/Windows/InputValuesWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using JXWPFToolkit.Controls;
+
+namespace JXWPFToolkit.Windows
+{
+	/// <summary>
+	/// Generic window used for asking the user to input some values
+	/// </summary>
+	public partial class InputValuesWindow
+	{
+		public InputValuesWindow(IEnumerable<InputValuesControl.InputItem> items, WindowSize size = WindowSize.Small, Orientation orientation = Orientation.Vertical, uint itemsPerRow = 1)
+		{
+			InitializeComponent();
+
+			AdjustWindowSize(size, orientation);
+
+			InputValues.SetItems(items, itemsPerRow);
+		}
+
+		#region events
+
+		/// <summary>
+		/// This event gets fired when the user has ended introducing values. It is sent when the window is closed.
+		/// </summary>
+		public event InputFinishedEventHandler InputFinished;
+
+
+		public delegate void InputFinishedEventHandler(object sender, InputFinishedEventArgs e);
+
+		public class InputFinishedEventArgs : EventArgs
+		{
+			public IEnumerable<InputValuesControl.InputItem> Items;
+		}
+
+		private void OnCloseClicked(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void OnSaveClicked(object sender, EventArgs e)
+		{
+			if (InputFinished == null)
+			{
+				return;
+			}
+
+			var args = new InputFinishedEventArgs();
+			args.Items = InputValues.GetItems();
+
+			InputFinished(this, args);
+		}
+
+		#endregion
+
+		private void AdjustWindowSize(WindowSize size, Orientation orientation)
+		{
+			switch (size)
+			{
+				case WindowSize.Small:
+					Height = 300;
+					Width = 350;
+					break;
+				case WindowSize.Medium:
+					Height = 400;
+					Width = 450;
+					break;
+				case WindowSize.Large:
+					Height = 500;
+					Width = 550;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException("size");
+			}
+
+			if (orientation == Orientation.Horizontal)
+			{
+				double aux = Height;
+				Height = Width;
+				Width = aux;
+			}
+		}
+
+
+	}
+
+
+	public enum WindowSize
+	{
+		Small,
+		Medium,
+		Large
+	}
+}
